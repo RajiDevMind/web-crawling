1a536642c0688786f748c47a3a345acd85f23b03
RajiDevMind <iopeyemi621@gmail.com>
2023-06-03 20:03:32 -1100

    crawling url in multiple input

--- crawl.test.js
+++ crawl.test.js
@@ -1,9 +1,30 @@
 const { normalizeUrl } = require("./crawl.js");
 const { test, expect } = require("@jest/globals");
 
-test("normalizeUrl", () => {
-  const input = "";
+test("normalizeUrl strip protocols", () => {
+  const input = "https://blog.boot.dev/path";
   const output = normalizeUrl(input);
-  const expected = "";
+  const expected = "blog.boot.dev/path";
+  expect(output).toEqual(expected);
+});
+
+test("normalizeUrl strip trailing slash", () => {
+  const input = "https://blog.boot.dev/path/";
+  const output = normalizeUrl(input);
+  const expected = "blog.boot.dev/path";
+  expect(output).toEqual(expected);
+});
+
+test("normalizeUrl caps", () => {
+  const input = "https://BLOG.boot.dev/path";
+  const output = normalizeUrl(input);
+  const expected = "blog.boot.dev/path";
+  expect(output).toEqual(expected);
+});
+
+test("normalizeUrl strip http", () => {
+  const input = "http://blog.boot.dev/path";
+  const output = normalizeUrl(input);
+  const expected = "blog.boot.dev/path";
   expect(output).toEqual(expected);
 });
